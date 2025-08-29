47fa6a0a8d156e5d0a04742cae03530d10304226
Vanness <vannesschia@gmail.com>
2025-08-29 18:48:00 -0400

    Change redirect route in Google OAuth

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `http://localhost:3000/auth/sign-in`,
+        redirectTo: `http://zprofile.tech/auth/sign-in`,
       },
     });
     if (error) {
