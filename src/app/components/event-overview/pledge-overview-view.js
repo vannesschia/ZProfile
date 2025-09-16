b9ed4fed0d1dfd9500566a13cce3a02d36bd46b2
Vanness <vannesschia@gmail.com>
2025-09-16 17:26:05 -0400

    Final changes

--- src/app/components/event-overview/pledge-overview-view.js
+++ src/app/components/event-overview/pledge-overview-view.js
@@ -1,28 +1,52 @@
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
-import { ProgressBlock } from "../progress-block"
-import { CoffeeChatsTable } from "../pledge-overview/coffee-chats/data-table"
-import { ChapterDataTable } from "../chapter/data-table"
-import { StudyTableDataTable } from "../pledge-overview/study-table/data-table"
+"use client"
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { CoffeeChatsTable } from "../pledge-overview/coffee-chats/data-table";
+import { ChapterDataTable } from "../chapter/data-table";
+import { StudyTableDataTable } from "../pledge-overview/study-table/data-table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-export default function PledgeOverviewView({ coffeeChats, pledgeEvents, studyTable }) {
+export default function PledgeOverviewView({
+  coffeeChats,
+  pledgeEvents,
+  studyTable,
+}) {
+  const router = useRouter();
   return (
-    <ProgressBlock title={"Pledge Overview"} subtext={"Here are events specific to the pledge class."}>
-      <Tabs defaultValue="coffeeChats">
-        <TabsList>
-          <TabsTrigger value="coffeeChats">Coffee Chats</TabsTrigger>
-          <TabsTrigger value="pledgeEvents">Pledge Events</TabsTrigger>
-          <TabsTrigger value="studyTables">Study Tables</TabsTrigger>
-        </TabsList>
-        <TabsContent value="coffeeChats">
-          <CoffeeChatsTable data={coffeeChats} />
-        </TabsContent>
-        <TabsContent value="pledgeEvents">
-          <ChapterDataTable data={pledgeEvents} />
-        </TabsContent>
-        <TabsContent value="studyTables">
-          <StudyTableDataTable data={studyTable} />
-        </TabsContent>
-      </Tabs>
-    </ProgressBlock>
-  )
+    <div className="w-full bg-background border-2 border-secondary p-6 rounded-lg">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+          <div className="flex flex-col gap-1">
+            <h2 className="text-2xl font-bold tracking-tight">Pledge Overview</h2>
+            <p className="text-sm tracking-tight">
+              Here are events specific to the pledge class.
+            </p>
+          </div>
+
+          <Button onClick={() => router.push('/coffee-chat')}>
+            Submit Coffee Chat <ArrowUpRight />
+          </Button>
+        </div>
+        <div>
+          <Tabs defaultValue="coffeeChats">
+            <TabsList>
+              <TabsTrigger value="coffeeChats">Coffee Chats</TabsTrigger>
+              <TabsTrigger value="pledgeEvents">Pledge Events</TabsTrigger>
+              <TabsTrigger value="studyTables">Study Tables</TabsTrigger>
+            </TabsList>
+            <TabsContent value="coffeeChats">
+              <CoffeeChatsTable data={coffeeChats} />
+            </TabsContent>
+            <TabsContent value="pledgeEvents">
+              <ChapterDataTable data={pledgeEvents} />
+            </TabsContent>
+            <TabsContent value="studyTables">
+              <StudyTableDataTable data={studyTable} />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
 }
