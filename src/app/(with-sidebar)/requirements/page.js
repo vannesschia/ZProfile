bc6a41a9c9b09aa004ba6e813ba0e649234dc982
georgu28 <georgu@umich.edu>
2026-02-04 21:24:27 -0500

    Updated Requirement Info for W26

--- src/app/(with-sidebar)/requirements/page.js
+++ src/app/(with-sidebar)/requirements/page.js
@@ -31,14 +31,15 @@ export default async function MembersPage() {
                                 <AccordionTrigger className="font-bold">Contacts and Links</AccordionTrigger>
                                 <AccordionContent>
                                     <ul className="list-disc pl-5 space-y-1">
-                                        <li>Nicky Nguyen: (224) 661-1514</li>
-                                        <li>Abby Moomaw: (919) 308-6644</li>
-                                        <li><u><strong><a href="https://docs.google.com/spreadsheets/d/1pTNPSq05jP9e0i8IEr0orUWKv9z_msK0c6Y-XQ9nmQc/edit?usp=sharing">Pledge Informational Sheets (Brother Contact, Coffee Chat Pairings, Cleanup Tracker)</a></strong></u></li>
-                                        <li><u><strong><a href="https://join.photocircleapp.com/S0C7Z2ZBXA">PhotoCircle</a></strong></u></li>
-                                        <li><u><strong><a href="https://calendar.google.com/calendar/embed?src=c_c813756b791ff495bc94dc7041d7e02a10136be55730bd669288ee8717a1bfd4%40group.calendar.google.com&ctz=America%2FNew_York">Google Calendar</a></strong></u></li>
-                                        <li><u><strong><a href="./coffee-chat">Coffee Chat Form</a></strong></u></li>
-                                        <li><u><strong><a href="https://drive.google.com/drive/folders/1_GnALj-7Gq_OYPatLA93B8vngQq5-v-8">Knowledgebase</a></strong></u></li>
-                                        <li><u><strong><a href="https://forms.gle/WfjH6i5gznAej1A87">Study Table Google Form</a></strong></u></li>
+                                        <li>Ananthu Nair: (734) 218-5735</li>
+                                        <li>Zachary Gammo: (248) 977-7628</li>
+                                        <li><u><strong><a href="https://docs.google.com/spreadsheets/d/1UonCbKMuklj9wzJTwvyKLXolxOviOu5VVU8WId56igo/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Pledge Attendance Tracker</a></strong></u></li>
+                                        <li><u><strong><a href="https://join.photocircleapp.com/3JFJHJDHXX" target="_blank" rel="noopener noreferrer">PhotoCircle</a></strong></u></li>
+                                        <li><u><strong><a href="https://calendar.google.com/calendar/embed?src=c_c813756b791ff495bc94dc7041d7e02a10136be55730bd669288ee8717a1bfd4%40group.calendar.google.com&ctz=America%2FNew_York" target="_blank" rel="noopener noreferrer">Google Calendar</a></strong></u></li>
+                                        <li><u><strong><a href="https://www.zprofile.tech/coffee-chat" target="_blank" rel="noopener noreferrer">Coffee Chat Form</a></strong></u></li>
+                                        <li><u><strong><a href="https://drive.google.com/drive/folders/1_GnALj-7Gq_OYPatLA93B8vngQq5-v-8" target="_blank" rel="noopener noreferrer">Knowledgebase</a></strong></u></li>
+                                        <li><u><strong><a href="https://forms.gle/WfjH6i5gznAej1A87" target="_blank" rel="noopener noreferrer">Study table google form</a></strong></u></li>
+                                        <li><u><strong><a href="https://groupme.com/join_group/110523698/OGCBP7U0" target="_blank" rel="noopener noreferrer">ZP Groupme</a></strong></u></li>
                                     </ul>
                                 </AccordionContent>
                             </AccordionItem>
@@ -49,12 +50,12 @@ export default async function MembersPage() {
                                 <AccordionContent className="space-y-2">
                                     <p>Weekly pledge events (~2 hours)</p>
                                     <ul className="list-disc pl-5 space-y-1">
-                                        <li>1 excused absence (notify Nicky or Abby ahead of time)</li>
+                                        <li>1 excused absence from pledge events</li>
+                                        <li>Must notify Ananthu or Zach ahead of time to use excused absence</li>
                                         <li>
-                                            Unexcused absences or &gt;1 excused absence must be made up with
-                                            <span className="font-medium"> either 2 coffee chats or committee points</span>.
+                                            Unexcused absences and more than one excused absence must be made up for with either 3 coffee chats or committee points.
                                         </li>
-                                        <li className="mt-1">Examples (any combo totaling 3 “points”):</li>
+                                        <li className="mt-1">This means you can do (any combination of the two to add up to 3 extra &quot;“points”):</li>
                                         <ul className="list-disc pl-8">
                                             <li>3 coffee chats</li>
                                             <li>3 committee events</li>
@@ -72,9 +73,10 @@ export default async function MembersPage() {
                                 <AccordionContent className="space-y-2">
                                     <p>Monthly chapter meetings (~1 hour)</p>
                                     <ul className="list-disc pl-5 space-y-1">
-                                        <li>One free excused absence (notify Nicky or Abby ahead of time)</li>
-                                        <li>Unexcused absence → 3 committee points</li>
-                                        <li>More than one excused absence → 2 committee points (each)</li>
+                                        <li>One free excused absence from chapter meetings</li>
+                                        <li>Must notify Ananthu or Zach ahead of time to use excused absence</li>
+                                        <li>Unexcused absence must be made up for with 3 committee points</li>
+                                        <li>More than one excused absence must be made up for with 2 committee points</li>
                                     </ul>
                                 </AccordionContent>
                             </AccordionItem>
@@ -84,15 +86,9 @@ export default async function MembersPage() {
                                 <AccordionTrigger className="font-bold">Committee Points</AccordionTrigger>
                                 <AccordionContent>
                                     <ul className="list-disc pl-5 space-y-1">
-                                        <li>6 committee points required before initiation</li>
+                                        <li>6 committee points before initiation across all 5 committees apart from RAM: Tech &amp; Projects, Professional Development, Marketing, Social, Fundraising</li>
                                         <li>
-                                            Points must come from these committees (RAM excluded):
-                                            <div className="ml-4 mt-1">
-                                                Tech &amp; Projects, Professional Development, Marketing, Social, Fundraising
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <span className="font-medium">Do not count</span>: parties, chapters, bid night, initiation night, pledge events
+                                            <span className="font-medium">Do not count</span>: parties, chapters, bid night, initiation night, and pledge events
                                         </li>
                                     </ul>
                                 </AccordionContent>
@@ -126,10 +122,10 @@ export default async function MembersPage() {
                                 <AccordionContent>
                                     <ul className="list-disc pl-5 space-y-1">
                                         <li>Must attend Zeta Pi pledge study table once a week for at least one hour</li>
-                                        <li>Will be held in Shapiro Tuesday and Thursday evenings from 7pm to 10pm</li>
+                                        <li>Will be held in Shapiro Tuesday and Wednesday evenings from 7pm to 10pm</li>
                                         <li>Pledges are in charge of finding a table or booking a room for the study session</li>
                                         <li>Submit proof of attending each week by submitting a selfie to the <strong><a href="https://forms.gle/WfjH6i5gznAej1A87">study table google form</a></strong></li>
-                                        <li>Allowed to miss one week throughout the semester without notifying Nicky or Abby at no penalty</li>
+                                        <li>Allowed to miss one week throughout the semester without notifying Ananthu or Zach at no penalty</li>
                                         <li>More than one missed study table must be made up for with either 1 committee point or 2 coffee chats.</li>
 
                                     </ul>
@@ -144,7 +140,7 @@ export default async function MembersPage() {
                                         <li>Must clean after a party once in addition to cleaning up after bid night</li>
                                         <li>Will be randomly assigned if there are not enough pledges signed up</li>
                                         <li>Supplies will be provided at house</li>
-                                        <li>View your cleanup points in the <strong><a href="https://docs.google.com/spreadsheets/d/17S7amIitZdCQx7JJhBYWi5hcAqG8UV2dOGO09GQFFVA/edit?usp=sharing">cleanup tracker</a></strong> in the pledge attendance sheet</li>
+                                        <li>View your cleanup points in the cleanup tracker in the <strong><a href="https://docs.google.com/spreadsheets/d/1UonCbKMuklj9wzJTwvyKLXolxOviOu5VVU8WId56igo/edit?usp=sharing" target="_blank" rel="noopener noreferrer">pledge attendance sheet</a></strong></li>
                                         <li>Failure to show up will result in additional coffee chats</li>
                                     </ul>
                                 </AccordionContent>
@@ -155,19 +151,32 @@ export default async function MembersPage() {
                                 <AccordionTrigger className="font-bold">Milestones</AccordionTrigger>
                                 <AccordionContent className="space-y-2">
                                     <p>
-                                        Deadlines to keep pledges on track. All requirements must be completed by initiation,
-                                        but these checkpoints help avoid penalties (e.g., social probation).
+                                        Deadlines by which pledges must fulfill certain requirements in order to avoid varying levels of punishments like social probation.
+                                        All requirements must be fulfilled by initiation but these milestones are created to keep pledges on track throughout their pledge semester.
                                     </p>
                                     <ul className="list-disc pl-5 space-y-1">
                                         <li>
-                                            <span className="font-semibold">Milestone 1:</span> 7 coffee chats · 2 committee points
+                                            <span className="font-semibold">Milestone 1 - February 27:</span> 7 coffee chats · 2 committee points
                                         </li>
                                         <li>
-                                            <span className="font-semibold">Milestone 2:</span> 14 coffee chats · 4 committee points
+                                            <span className="font-semibold">Milestone 2 - March 27:</span> 14 coffee chats · 4 committee points
                                         </li>
                                         <li>
-                                            <span className="font-semibold">Milestone 3:</span> 20 coffee chats · 6 committee points
+                                            <span className="font-semibold">Milestone 3 - April 21:</span> 20 coffee chats · 6 committee points
                                         </li>
+                                    </ul>
+                                </AccordionContent>
+                            </AccordionItem>
+
+                            {/* Pledge Class President */}
+                            <AccordionItem value="pcp">
+                                <AccordionTrigger className="font-bold">Pledge Class President</AccordionTrigger>
+                                <AccordionContent>
+                                    <ul className="list-disc pl-5 space-y-1">
+                                        <li>One pledge will have the chance to be elected as PCP during an election that will be held after UGLI Challenge on Zoom: Wednesday 2/18</li>
+                                        <li>Sends hype messages for stables, pledge events, parties</li>
+                                        <li>Eboard Meetings: Call in for 20 minutes and act as voice for pledges</li>
+                                        <li>Ask questions/express concerns on behalf of PC</li>
                                     </ul>
                                 </AccordionContent>
                             </AccordionItem>
@@ -188,11 +197,11 @@ export default async function MembersPage() {
                                 <AccordionTrigger className="font-bold">Contacts and Links</AccordionTrigger>
                                 <AccordionContent>
                                     <ul className="list-disc pl-5 space-y-1">
-                                        <li>Nicky Nguyen: (224) 661-1514</li>
-                                        <li>Abby Moomaw: (919) 308-6644</li>
-                                        <li><u><strong><a href="https://join.photocircleapp.com/S0C7Z2ZBXA">PhotoCircle</a></strong></u></li>
-                                        <li><u><strong><a href="https://calendar.google.com/calendar/embed?src=c_c813756b791ff495bc94dc7041d7e02a10136be55730bd669288ee8717a1bfd4%40group.calendar.google.com&ctz=America%2FNew_York">Google Calendar</a></strong></u></li>
-                                        <li><u><strong><a href="https://drive.google.com/drive/folders/1_GnALj-7Gq_OYPatLA93B8vngQq5-v-8">Knowledgebase</a></strong></u></li>
+                                        <li>Ananthu Nair: (734) 218-5735</li>
+                                        <li>Zachary Gammo: (248) 977-7628</li>
+                                        <li><u><strong><a href="https://join.photocircleapp.com/3JFJHJDHXX" target="_blank" rel="noopener noreferrer">PhotoCircle</a></strong></u></li>
+                                        <li><u><strong><a href="https://calendar.google.com/calendar/embed?src=c_c813756b791ff495bc94dc7041d7e02a10136be55730bd669288ee8717a1bfd4%40group.calendar.google.com&ctz=America%2FNew_York" target="_blank" rel="noopener noreferrer">Google Calendar</a></strong></u></li>
+                                        <li><u><strong><a href="https://drive.google.com/drive/folders/1_GnALj-7Gq_OYPatLA93B8vngQq5-v-8" target="_blank" rel="noopener noreferrer">Knowledgebase</a></strong></u></li>
                                     </ul>
                                 </AccordionContent>
                             </AccordionItem>
